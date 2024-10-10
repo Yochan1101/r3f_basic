4972a8bae074393836e7b856dc62afd63cf09426
ojyochan <jych2365@gmail.com>
2024-10-10 12:51:53 +0900

    그룹으로 움직이기 (world, local 좌표계)

--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,30 +1,79 @@
 import * as THREE from "three";
-import { useFrame, useThree } from "@react-three/fiber";
-// import { useRef } from "react";
+// import { useThree, useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 export default function ThreeElement() {
-  // const { size, gl, scene, camera } = useThree();
-  // const boxRef = useRef<THREE.Mesh>(null);
+  // const { size, gl, scene, camera } = useThree(); // This will just crash
+  const boxRef = useRef<THREE.Mesh>(null);
+  const groupRef = useRef<THREE.Group>(null);
 
   // useFrame((state, delta) => {
   //   // boxRef.current.rotation.x += delta;
-  //   // boxRef.current.position.y += delta;
-  //   // boxRef.current.position.x += delta;
+  //   // boxRef.current.position.x += 0.01;
+  //   // boxRef.current.scale.x += 0.01;
+  //   // scene.position.x += 0.01;
+  //   // scene.rotation.x += 0.01;
+  //   // groupRef.current.rotation.x += delta;
   // });
 
   return (
     <>
       <directionalLight position={[5, 5, 5]} />
-      <mesh
-        rotation={[THREE.MathUtils.degToRad(0), THREE.MathUtils.degToRad(0), 0]} // 매쉬안에 있는 박스 각도 변경
-        position={[2, 0, 0]} // 매쉬안에서 어디에 위치할지
-        scale={[1, 6, 1]} // 크기 변경
+      <group
+        ref={groupRef}
+        position={[0, 0, 0]}
+        rotation={[
+          THREE.MathUtils.degToRad(0),
+          THREE.MathUtils.degToRad(45),
+          THREE.MathUtils.degToRad(0),
+        ]}
       >
-        <boxGeometry />1
-        <meshStandardMaterial color="blue" />
-      </mesh>
+        <axesHelper args={[5]} />
+        <mesh
+          ref={boxRef}
+          position={[0, 0, 0]}
+          scale={[1, 1, 1]}
+          rotation={[
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+          ]}
+        >
+          <boxGeometry />
+          <meshStandardMaterial color="red" />
+        </mesh>
+        <mesh
+          ref={boxRef}
+          position={[0, 2, 0]}
+          scale={[1, 1, 1]}
+          rotation={[
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(0),
+          ]}
+        >
+          <boxGeometry />
+          <meshStandardMaterial color="green" />
+        </mesh>
+        <mesh
+          ref={boxRef}
+          position={[2, 0, 0]}
+          scale={[1, 1, 1]}
+          rotation={[
+            THREE.MathUtils.degToRad(0),
+            THREE.MathUtils.degToRad(45),
+            THREE.MathUtils.degToRad(45),
+          ]}
+        >
+          <axesHelper args={[3]} />
+          <boxGeometry />
+          <meshStandardMaterial color="blue" />
+        </mesh>
+      </group>
     </>
   );
 }
-
 // 매초마다 프레임을 제공
+// group으로 mesh를 묶어서 사용할 수 있음
+// group으로 이루어진 scene은 월드 좌표계
+// 기본 mesh의 각도는 local 좌표계
