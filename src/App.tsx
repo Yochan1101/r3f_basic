fe899fde5bca84b34dab80701e0bdb9ee38eb04f
ojyochan <jych2365@gmail.com>
2024-10-10 12:07:33 +0900

    R3F 설명 강의 끝

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,45 @@
 import { Canvas } from "@react-three/fiber";
 import "./App.css";
 import ThreeElement from "./ThreeElement";
-
+import { OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
 function App() {
+  const color = useControls({
+    value: "white",
+  });
+
+  const grid = useControls({
+    segment: { value: 10, min: 2, max: 100, step: 1 },
+  });
+
   return (
     <>
-      <Canvas>
+      <Canvas
+        // orthographic
+        camera={{
+          fov: 75,
+          near: 1,
+          far: 100,
+          position: [3, 3, 0],
+          // zoom: 100,
+        }}
+      >
+        <color attach={"background"} args={[color.value]} />
+        <OrbitControls /> // 자유로운 회전이 가능하도록 하는 제약을 줄 수 있는
+        args가 존재 문서 찾아보면됨 개발할때는 쓸일 없을듯, 사용자에게 어느
+        화면은 안보여주고 싶거나 할때 사용
+        <axesHelper args={[5]} /> // xyz축이 보이도록 하는
+        <gridHelper args={[10, grid.segment]} /> // 말그대로 화면에 그리드를
+        그려줌
         <ThreeElement />
       </Canvas>
-      <h1>R3F BASIC</h1>
     </>
   );
 }
 
 export default App;
+
+// camera 옵션 NEAR(카메라 중간), FAR(카메라 맨끝), FOV(확대, 각도 나타냄),
+
+// perspective camera (원근 카메라, 투시카메라) pov를 사용하여 zoom
+//orthographic camera (직교 카메라, 직교투시카메라) 2D zoom을 사용하여 확대
--- src/ThreeElement.tsx
+++ src/ThreeElement.tsx
@@ -1,19 +1,36 @@
 import * as THREE from "three";
+import { useControls } from "leva";
+// import { useFrame, useThree } from "@react-three/fiber";
+// import { useRef } from "react";
 
 export default function ThreeElement() {
+  // const { size, gl, scene, camera } = useThree();
+  // const boxRef = useRef<THREE.Mesh>(null);
+
+  // useFrame((state, delta) => {
+  //   // boxRef.current.rotation.x += delta;
+  //   // boxRef.current.position.y += delta;
+  // });
+
+  const box = useControls({
+    roatate: { value: 0, min: -360, max: 360, step: 1 },
+  });
+
   return (
     <>
-      <directionalLight position={[0, 5, 5]} />
+      <directionalLight position={[5, 5, 5]} />
       <mesh
         rotation={[
           THREE.MathUtils.degToRad(45),
-          THREE.MathUtils.degToRad(45),
+          THREE.MathUtils.degToRad(box.roatate),
           0,
         ]}
       >
-        <boxGeometry />
+        <boxGeometry />1
         <meshStandardMaterial color="blue" />
       </mesh>
     </>
   );
 }
+
+// 매초마다 프레임을 제공
